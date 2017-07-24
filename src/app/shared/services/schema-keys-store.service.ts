f89e534a6f5a078848862dc66ff8e64d7e62dede
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2017-07-24 14:09:12 +0200

    schema-keystore: Refactor service
    Signed-off-by: Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>

--- src/app/shared/services/schema-keys-store.service.ts
+++ src/app/shared/services/schema-keys-store.service.ts
@@ -1,37 +1,56 @@
 import { Injectable } from '@angular/core';
 import { fromJS, OrderedSet } from 'immutable';
+import * as _ from 'lodash';
 
 @Injectable()
 export class SchemaKeysStoreService {
 
-  private schemaSeparator = '/';
-  public keyStoreMap: { [path: string]: OrderedSet<string> } = {};
+  public separator = '/';
+  public schemaKeyStoreMap: { [path: string]: OrderedSet<string> } = {};
 
   constructor() { }
+
+  public forPath(path: string) {
+    if (path === '') {
+      return this.schemaKeyStoreMap[''].toArray();
+    }
+    return this.schemaKeyStoreMap[`${this.separator}${path}`] ? this.schemaKeyStoreMap[`${this.separator}${path}`].toArray() : [];
+  }
 
   public buildSchemaKeyStore(schema: {}) {
     this.buildSchemaKeyStoreRecursively('', schema);
   }
 
   private buildSchemaKeyStoreRecursively(path: string, schema: {}) {
-    if (schema['items']) {
-      schema = schema['items'];
+
+    if (schema['type'] === 'object') {
+      let finalKeys = Object.keys(schema['properties']);
+      this.schemaKeyStoreMap[path] = fromJS(finalKeys).toOrderedSet();
+      finalKeys
+      .filter(key => this.isObjectOrArraySchema(schema['properties'][key]))
+      .forEach(key => {
+        let newPath = `${path}${this.separator}${key}`;
+        this.buildSchemaKeyStoreRecursively(newPath, schema['properties'][key]);
+      });
+
     }
 
-    schema = schema['properties'];
-
-    let finalKeys = Object.keys(schema);
-    this.keyStoreMap[path] = fromJS(finalKeys).toOrderedSet();
-    finalKeys
-    .filter(key => this.isObjectOrArraySchema(schema[key]))
-    .forEach(key => {
-      let newPath = `${path}${this.schemaSeparator}${key}`;
-      this.buildSchemaKeyStoreRecursively(newPath, schema[key]);
-    });
-
+    if (schema['type'] === 'array') {
+      if (schema['items']['type'] === 'object') {
+        let finalKeys = Object.keys(schema['items']['properties']);
+        this.schemaKeyStoreMap[path] = fromJS(finalKeys).toOrderedSet();
+        finalKeys
+        .filter(key => this.isObjectOrArraySchema(schema['items']['properties'][key]))
+        .forEach(key => {
+          let newPath = `${path}${this.separator}${key}`;
+          this.buildSchemaKeyStoreRecursively(newPath, schema['items']['properties'][key]);
+        });
+      }
+    }
   }
 
   private isObjectOrArraySchema(schema: {}) {
     return schema['type'] === 'object' || schema['type'] === 'array';
   }
+
 }
