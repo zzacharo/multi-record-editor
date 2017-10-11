372e56d04b316778e3a98644d17d93e613140124
Glignos <glignos93@gmail.com>
2017-10-11 15:46:42 +0200

    create the actions template
    * Create the template for user actions and conditions
      (closes #12)
    
    Signed-off-by: Glignos <glignos93@gmail.com>

--- src/app/conditions-template/conditions-template.component.ts
+++ src/app/conditions-template/conditions-template.component.ts
@@ -8,17 +8,11 @@ import { AppGlobalsService } from '../shared/services';
   styleUrls: ['./conditions-template.component.scss']
 })
 export class ConditionsTemplateComponent {
-  @Input() id: number;
   @Input() condition: Condition;
-  @Output() onConditionDeleted = new EventEmitter<number>();
   constructor(private appGlobalsService: AppGlobalsService) { }
 
   get matchTypes(): string[] {
     return this.appGlobalsService.matchTypes;
-  }
-
-  deleteElement() {
-    this.onConditionDeleted.emit(this.id);
   }
 
   trackByFunction(index: number): number {
