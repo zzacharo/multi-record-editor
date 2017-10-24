Glignos/ng2-multi-record-editor

tag-filter: Add json filtering by schema path

src/app/shared/pipes/tag-filter.pipe.spec.ts
@@ -1,0 +1,80 @@
+import { TagFilterPipe } from './tag-filter.pipe';
+import { JsonUtilsService } from '../services/json-utils.service';
+import { SchemaKeysStoreService } from '../services/schema-keys-store.service';
+
+describe('TagFilterPipe', () => {
+  it(`filters records by the fields specified. It returns the union
+    of all filters applied to each record.`, () => {
+    const pipe = new TagFilterPipe(new JsonUtilsService(new SchemaKeysStoreService()));
+    let records = [{
+      lla: 3,
+      authors: [
+        {
+          affiliations: [
+            {
+              value:'inst3',
+              name: 'inst3'
+            },
+            {
+              value:'inst4',
+              name: 'inst4'
+            }
+          ]
+        }
+      ],
+      test: 4,
+      dup: 3
+    }, {
+    authors: [
+      {
+        affiliations: [
+          {
+            value:'inst3',
+            name: 'inst3'
+          },
+          {
+            value:'inst4',
+            name: 'inst4'
+          }
+        ]
+      }, {
+        affiliations: [
+          {
+            name: 'inst4'
+          },
+          {
+            name: 'inst5'
+          }
+        ],
+        full_name: 'dummy'
+      }
+    ]}];
+
+    let expected = [{
+      authors: [{
+          affiliations: [
+            {
+              value:'inst3'
+            },
+            {
+              value:'inst4'
+            }
+          ]
+        }
+      ]}, {
+      authors: [{
+        affiliations: [
+          {
+            value:'inst3'
+          },
+          {
+            value:'inst4'
+          }
+        ]
+      }]
+    }];
+    let result = pipe.transform(records, ['authors/affiliations/value']);
+    console.log(result);
+    expect(result).toEqual(expected);
+  });
+});

src/app/shared/pipes/tag-filter.pipe.ts
@@ -1,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
+import { JsonUtilsService } from '../services/json-utils.service';
+
+
+@Pipe({
+  name: 'tagFilter'
+})
+export class TagFilterPipe implements PipeTransform {
+
+  constructor(public jsonUtilsService: JsonUtilsService) { }
+
+  transform(records: Array<{}>, filterExpression: string): any {
+    if (filterExpression) {
+      return this.jsonUtilsService.filterJsonArray(records, filterExpression);
+    }
+    return records;
+  }
+
+}

src/app/shared/services/json-utils.service.spec.ts
@@ -1,0 +1,110 @@
+import { JsonUtilsService } from './json-utils.service';
+import { SchemaKeysStoreService } from './schema-keys-store.service';
+
+describe('JsonUtilsService', () => {
+  it('filters a nested complex object given a path and returns the object keeping the object structure.', () => {
+    const service = new JsonUtilsService(new SchemaKeysStoreService());
+    let record = {
+      authors: [
+        {
+          affiliations: [
+            {
+              value:'inst1',
+              name: 'inst1'
+            },
+            {
+              value:'inst2',
+              name: 'inst2'
+            }
+          ]
+        },
+        {
+          affiliations: [
+            {
+              value:'inst3',
+              name: 'inst3'
+            },
+            {
+              value:'inst4',
+              name: 'inst4'
+            }
+          ],
+          full_name: 'dummy'
+        }
+      ]
+    };
+
+    let expected = {
+      authors: [
+        {
+          affiliations: [
+            {
+              value:'inst1'
+            },
+            {
+              value:'inst2'
+            }
+          ]
+        },
+        {
+          affiliations: [
+            {
+              value:'inst3'
+            },
+            {
+              value:'inst4'
+            }
+          ],
+          full_name: 'dummy'
+        }
+      ]
+    };
+
+    let result = service.filterJson(record, ['authors', 'affiliations', 'value', 'authors', 'full_name']);
+    expect(result).toEqual(expected);
+  });
+  it('filters a nested complex object given a path by converting to tree and back', () => {
+    const service = new JsonUtilsService(new SchemaKeysStoreService());
+    let record = {
+      authors: [
+        {
+          affiliations: [
+            {
+              value:'inst1',
+              name: 'inst1'
+            },
+            {
+              value:'inst2',
+              name: 'inst2'
+            }
+          ]
+        },
+        {
+          affiliations: [
+            {
+              name: 'inst3'
+            }
+          ],
+          full_name: 'dummy'
+        }
+      ]
+    };
+    let expected = {
+      authors: [{
+        affiliations: [
+          {
+            value:'inst1'
+          },
+          {
+            value:'inst2'
+          }
+        ]
+      }, {
+        full_name: 'dummy'
+      }
+    ]};
+    let tags = ['authors/affiliations/value', 'authors/full_name'];
+    let result = service.filterJson(record, tags);
+    expect(result).toEqual(expected);
+  })
+});

zzacharo/multi-record-editor

src/app/shared/pipes/tag-filter.pipe.spec.ts
@@ -1,0 +1,79 @@
+import { TagFilterPipe } from './tag-filter.pipe';
+import { JsonUtilsService } from '../services/json-utils.service';
+import { SchemaKeysStoreService } from '../services/schema-keys-store.service';
+
+describe('TagFilterPipe', () => {
+  it(`filters records by the fields specified. It returns the union
+    of all filters applied to each record.`, () => {
+    const pipe = new TagFilterPipe(new JsonUtilsService(new SchemaKeysStoreService()));
+    let records = [{
+      lla: 3,
+      authors: [
+        {
+          affiliations: [
+            {
+              value: 'inst3',
+              name: 'inst3'
+            },
+            {
+              value: 'inst4',
+              name: 'inst4'
+            }
+          ]
+        }
+      ],
+      test: 4,
+      dup: 3
+    }, {
+    authors: [
+      {
+        affiliations: [
+          {
+            value: 'inst3',
+            name: 'inst3'
+          },
+          {
+            value: 'inst4',
+            name: 'inst4'
+          }
+        ]
+      }, {
+        affiliations: [
+          {
+            name: 'inst4'
+          },
+          {
+            name: 'inst5'
+          }
+        ],
+        full_name: 'dummy'
+      }
+    ]}];
+
+    let expected = [{
+      authors: [{
+          affiliations: [
+            {
+              value: 'inst3'
+            },
+            {
+              value: 'inst4'
+            }
+          ]
+        }
+      ]}, {
+      authors: [{
+        affiliations: [
+          {
+            value: 'inst3'
+          },
+          {
+            value: 'inst4'
+          }
+        ]
+      }]
+    }];
+    let result = pipe.transform(records, 'authors/affiliations/value');
+    expect(result).toEqual(expected);
+  });
+});

src/app/shared/pipes/tag-filter.pipe.ts
@@ -1,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
+import { JsonUtilsService } from '../services/json-utils.service';
+
+
+@Pipe({
+  name: 'tagFilter'
+})
+export class TagFilterPipe implements PipeTransform {
+
+  constructor(public jsonUtilsService: JsonUtilsService) { }
+
+  transform(records: Array<{}>, filterExpression: string): any {
+    if (filterExpression) {
+      return this.jsonUtilsService.filterObjectArray(records, filterExpression);
+    }
+    return records;
+  }
+
+}

src/app/shared/services/json-utils.service.spec.ts
@@ -1,0 +1,111 @@
+import { JsonUtilsService } from './json-utils.service';
+import { SchemaKeysStoreService } from './schema-keys-store.service';
+
+describe('JsonUtilsService', () => {
+  it('filters a nested complex object given a path and returns the object keeping the object structure.', () => {
+    const service = new JsonUtilsService(new SchemaKeysStoreService());
+    let record = {
+      authors: [
+        {
+          affiliations: [
+            {
+              value: 'inst1',
+              name: 'inst1'
+            },
+            {
+              value: 'inst2',
+              name: 'inst2'
+            }
+          ]
+        },
+        {
+          affiliations: [
+            {
+              value: 'inst3',
+              name: 'inst3'
+            },
+            {
+              value: 'inst4',
+              name: 'inst4'
+            }
+          ],
+          full_name: 'dummy'
+        }
+      ]
+    };
+
+    let expected = {
+      authors: [
+        {
+          affiliations: [
+            {
+              value: 'inst1'
+            },
+            {
+              value: 'inst2'
+            }
+          ]
+        },
+        {
+          affiliations: [
+            {
+              value: 'inst3'
+            },
+            {
+              value: 'inst4'
+            }
+          ],
+          full_name: 'dummy'
+        }
+      ]
+    };
+
+    let result = service.filterObject(record, ['authors/affiliations/value', 'authors/full_name']);
+    expect(result).toEqual(expected);
+  });
+
+  it('filters a nested complex object given a path by converting to tree and back', () => {
+    const service = new JsonUtilsService(new SchemaKeysStoreService());
+    let record = {
+      authors: [
+        {
+          affiliations: [
+            {
+              value: 'inst1',
+              name: 'inst1'
+            },
+            {
+              value: 'inst2',
+              name: 'inst2'
+            }
+          ]
+        },
+        {
+          affiliations: [
+            {
+              name: 'inst3'
+            }
+          ],
+          full_name: 'dummy'
+        }
+      ]
+    };
+    let expected = {
+      authors: [{
+        affiliations: [
+          {
+            value: 'inst1'
+          },
+          {
+            value: 'inst2'
+          }
+        ]
+      }, {
+        full_name: 'dummy'
+      }
+    ]};
+    let tags = ['authors/affiliations/value', 'authors/full_name'];
+    let result = service.filterObject(record, tags);
+    expect(result).toEqual(expected);
+  });
+});