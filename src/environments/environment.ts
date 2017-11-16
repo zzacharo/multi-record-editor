a190ea0ba4d4170bfec9f8771c6c0436d54d0601
harunurhan <harunurhan17@gmail.com>
2017-11-16 15:49:14 +0100

    setup sentry with sourcemap deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:5000'
+  baseUrl: 'http://localhost:5000',
+  sentryPublicDSN: null,
+  version: null,
 };
---
+++ src/environments/version.ts
@@ -0,0 +1,2 @@
+// actual version will be written by `write-version.js` script before deploy
+export const version = '';
